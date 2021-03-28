2e2bfd0a0ddb3bb0c6873947dccb727968e748c4
LeoSanGo <souoleo@gmail.com>
2021-03-28 19:10:27 -0300

    Tratando erros e automatizando dados da api de cotação

--- teste-api.js
+++ teste-api.js
@@ -3,5 +3,6 @@ const axios = require('axios');
 const url = `https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao='03-26-2021'&$top=100&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao`;
 
 axios
-.get(url)
-.then( res => console.log(res.data.value[0].cotacaoVenda));
+    .get(url)
+    .then( res => console.log(res.data.value[0].cotacaoVenda))
+    .catch( err => console.log(err));
