9c99bf54516304ecfd7755d357c50fe09e4042c5
LeoSanGo <souoleo@gmail.com>
2021-03-28 12:30:27 -0300

    Modificado conversor, CSS, bug NaN calculo

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const convert = require('./lib/convert.js')
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -9,6 +11,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.get('/cotacao', (req, res) => {
+    const { cotacao, quantidade } = req.query;
+    const conversao = convert.convert(cotacao, quantidade);
+    res.render('cotacao', {
+        cotacao: convert.toMoney(cotacao),
+        quantidade: convert.toMoney(quantidade),
+        conversao: convert.toMoney(conversao)
+    });
+
 });
 
 app.listen(3000, err => {
