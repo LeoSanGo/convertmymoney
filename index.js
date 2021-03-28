ab2c4afa9ea0bd2bfb5342061a14692db6f46bcd
LeoSanGo <souoleo@gmail.com>
2021-03-28 10:23:41 -0300

    Adicionado index.js, home.ejs

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.listen(3000, err => {
+    if(err) {
+        console.log('Não foi possivel iniciar');
+    } else {
+        console.log('ConvertMyMoney esta online');
+    }
+});
